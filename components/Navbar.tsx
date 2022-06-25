75943bf100b2b8999e9ba087663040cd2a68b972
Vaibhav Kubre <v@kubre.in>
2022-06-25 08:18:56 +0530

    Refactored nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,37 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Navbar  = () => {
+const Navbar = () => {
   return (
     <nav className="py-4 flex text-center justify-around md:justify-end px-8 gap-x-2 md:gap-x-8">
-      <Link href="/">
-        <a className="py-2 px-2 w-full md:w-36 uppercase rounded bg-slate-50 text-slate-900 transition hover:bg-slate-50 hover:text-slate-900 hidden md:inline">
-          Home
-        </a>
-      </Link>
-      <Link href="/portfolio">
-        <a className="py-2 px-2 w-full md:w-36 uppercase rounded transition hover:bg-slate-50 hover:text-slate-900">
-          Portfolio
-        </a>
-      </Link>
-      <Link href="/blog">
-        <a className="py-2 px-2 w-full md:w-36 uppercase rounded transition hover:bg-slate-50 hover:text-slate-900">
-          Blog
-        </a>
-      </Link>
-      <Link href="/resume">
-        <a className="py-2 px-2 w-full md:w-36 uppercase rounded transition hover:bg-slate-50 hover:text-slate-900">
-          Resume
-        </a>
-      </Link>
+      <NavItem title="Home" href="/" />
+      <NavItem title="Portfolio" href="/portfolio" />
+      <NavItem title="Blog" href="/blog" />
     </nav>
   );
 };
 
 export default Navbar;
+
+interface NavItemProps {
+  title: string;
+  href: string;
+}
+
+const NavItem = ({ title, href }: NavItemProps) => {
+  const router = useRouter();
+  const isActive = router.pathname === href;
+  const activeClasses = "bg-slate-50 text-slate-900";
+
+  return (
+    <Link href={href}>
+      <a
+        className={`${
+          isActive && activeClasses
+        } py-2 px-2 w-full md:w-36 uppercase rounded transition hover:bg-slate-50 hover:text-slate-900`}
+      >
+        {title}
+      </a>
+    </Link>
+  );
+};
